64d6756af9e6a50d0cd5cd1c5fd3c35442452ef4
william jacobs <williamjacobs@williams-MacBook-Pro.local>
2020-12-11 18:44:03 -0800

    fixed the font awesome bs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookDead } from '@fortawesome/free-solid-svg-icons'
+
 
 const Header = () => {
     return (
         <header>
-            <i className="fas fa-book-dead"></i>
+            <FontAwesomeIcon size="6x" icon={faBookDead} />            
             <h1>DeadBooks</h1>
         </header>
     )
